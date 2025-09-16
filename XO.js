94e0f3aa97efe2143204eba927ede93c32ab332a
noursaadaldeen66-source <nour.saadaldeen66@gmail.com>
2025-09-16 01:27:19 -0900

    XO 80%

--- XO.js
+++ XO.js
@@ -3,34 +3,50 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-let myArray = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let elementCount = 0;
+let massge = "XMAN CHOSE PLACE";
 
 function askForElement() {
-  readline.question(` START ${elementCount + 1}: `, (input) => {
-    if (input.trim() === "") {
-      console.log("الرجاء إدخال قيمة صالحة!");
-      askForElement();
-      return;
+  let count = 1;
+  if (count % 2 == 0) massge = "OMAN CHOSE PLACE";
+  readline.question(
+    ` Play X: ${myArray.splice(0, 3)} \n \t \t ${myArray.splice(
+      0,
+      3
+    )}  \n \t \t ${myArray.splice(0, 9)} \n ${massge}: `,
+    (input) => {
+      if (input.trim() === "") {
+        if (count % 2 != 0 && parseInt(input) == /[1-9]/.test) {
+          massge = "OMAN CHOSE PLACE";
+          count++;
+        } else if (count % 2 == 0 && parseInt(input) == /[1-9]/.test) {
+          count++;
+        } else if (myArray[parseInt(input) - 1] === String)
+          console.log("Wrong CHOSE");
+        askForElement();
+        return;
+      }
+
+      if (count % 2 != 0) {
+        myArray[parseInt(input) - 1] = "X";
+      } else if (count % 2 == 0) {
+        myArray[parseInt(input) - 1] = "O";
+      }
+
+      //check
+
+      elementCount++;
+
+      if (elementCount < 10) {
+        askForElement();
+      } else {
+        console.log("NO WINER");
+
+        readline.close();
+      }
     }
-
-    myArray.push(input);
-    elementCount++;
-
-    console.log(`تم إضافة العنصر "${input}" بنجاح!`);
-    console.log(`المصفوفة الحالية: [${myArray.join(", ")}]`);
-
-    if (elementCount < 2) {
-      askForElement();
-    } else {
-      console.log("تم إدخال العنصرين!");
-      readline.close();
-    }
-  });
+  );
 }
 
 askForElement();
