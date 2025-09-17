ff55ba76876f3d83a1366f86719b6b478ac9a73c
noursaadaldeen66-source <nour.saadaldeen66@gmail.com>
2025-09-17 12:43:06 +0300

    arry method

--- TenTaskMed.js
+++ TenTaskMed.js
@@ -251,3 +251,61 @@
 //تم استخدام ال ai ولكن توفير للوقت ولعدم معرفة بتعليمات الاسكي ضمن فكرتي
 
 ///////////////////////
+
+
+
+// const students =[
+//     {name : farah ,grad:85},
+//     {name : nour ,grad:70},
+//     {name : hamzah ,grad:60},
+//     {name : jojo ,grad:65},
+// ];
+
+// const sort =(arr) =>{
+    
+// }
+
+
+
+let fullName = ["Nour Saad", "Jol Isso" , "Hanano Panada"];
+
+
+let arr = fullName.map((item) => (item.split(" ")[0].slice(0 ,1) + "." + item.split(" ")[1].slice(0 ,1)) );
+
+console.log(arr);
+
+
+
+let input=[1,11,-9,20,-3,8,-1];
+
+let arr3 = input.reduce((a,b) =>( b> 0 ? a+b: a) );
+console.log(arr3);
+
+
+let inputS =[
+    {name:"nour" , grad:85},
+    {name:"mahmo" , grad:40},
+    {name:"ahmad" , grad:60},
+    {name:"hamzah" , grad:50}
+];
+
+let studentPass = inputS.filter((item) => item.grad >= 60 ).map((item) =>  {
+ return item.name ;  
+    
+} );
+
+console.log(studentPass);
+
+
+
+let sorti =[
+    {name: "Ahmad" ,age: 25},
+    {name: "jon" ,age: 30},
+    {name: "rama" ,age: 22},
+]
+
+let namea = sorti.sort( ( a,b ) => b.age - a.age  );
+let namee = sorti.map((item) => item.name );
+
+// console.log(namea);
+console.log(namee);
